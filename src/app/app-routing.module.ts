8be10d06e82e223dd2295a1436334d9836221f78
Lavaca <ezequiel.o.lavaca@accenture.com>
2020-05-29 17:05:45 -0300

    arquitectura básica

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogOutComponent } from './components/log-out/log-out.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'home', 
+    component:  HomeComponent
+  },
+  { 
+    path: 'login', 
+    component: LoginComponent
+  },
+  { 
+    path: 'log-out',
+    component: LogOutComponent
+  },
+  {
+    path: '',   
+    redirectTo: '/home', 
+    pathMatch: 'full'
+  },
+  { 
+    path: '**', 
+    component: NotfoundComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { LogOutComponent } from './components/log-out/log-out.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './modules/core/core.module';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    LogOutComponent
+    LogOutComponent,
+    HomeComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    CoreModule,
     AppRoutingModule
   ],
   providers: [],
