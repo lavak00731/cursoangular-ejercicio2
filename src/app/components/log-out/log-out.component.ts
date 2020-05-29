5b380dc590b67e27914193350ce561894baf58fc
Lavaca <ezequiel.o.lavaca@accenture.com>
2020-05-29 20:06:55 -0300

    logout logic

--- src/app/components/log-out/log-out.component.ts
+++ src/app/components/log-out/log-out.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserType } from './../../interfaces/user-type';
+import { AuthApiServiceService } from './../../services/auth-api-service.service';
 
 @Component({
   selector: 'app-log-out',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./log-out.component.scss']
 })
 export class LogOutComponent implements OnInit {
-
-  constructor() { }
-
+  userInfo: UserType = {
+    name: "",
+    password: ""
+  }
+  constructor(private _loggedUser: AuthApiServiceService, private _snackBar: MatSnackBar ) { }
+  showFeedback (message, action) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+    });
+  }
   ngOnInit(): void {
+    if (this._loggedUser !== undefined) {
+      this._loggedUser.setUserInfo(this.userInfo);
+      this.showFeedback('You are not', 'logged anymore!');
+    }
   }
 
 }
